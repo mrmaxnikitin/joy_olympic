f464b329cb00e1f64ed2d2db34f930aa653563f3
Max Nikitin <mr.maxnikitin@yandex.ru>
2017-01-02 03:22:55 +0300

    добавил метрику

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,28 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Статистика" do
+          yandex_link = <<-LINK
+            <!-- Yandex.Metrika informer -->
+<a href="https://metrika.yandex.ru/stat/?id=41916344&amp;from=informer"
+target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/41916344/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="41916344" data-lang="ru" /></a>
+<!-- /Yandex.Metrika informer -->
+          LINK
+          yandex_link.html_safe
+        end
       end
+
+      column do
+        panel "Пользователи" do
+          h4 "Активные #{User.count}"
+        end
+      end
+
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end # content
+
 end
