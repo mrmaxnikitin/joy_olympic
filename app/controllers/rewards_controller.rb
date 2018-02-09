453fa559378e936e1549cf416bfcf57bde0b073d
Max Nikitin <mr.maxnikitin@yandex.ru>
2018-02-09 13:26:46 +0300

    1500 in results

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -3,7 +3,7 @@ class RewardsController < ApplicationController
   before_action :require_admin, only: [:activate_diploma]
 
 	def index
-		@rewards = Reward.order(created_at: "DESC").limit(4000)
+		@rewards = Reward.order(created_at: "DESC").limit(1500)
 	end
 
 	def show
